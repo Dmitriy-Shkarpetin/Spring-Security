1da86e7cd35d611c22fcc7171cef6caa157f888c
Dmitriy_Shkarpetin <dimhik.shkarpetin@gmail.com>
2022-07-19 20:37:18 +0300

    first

---
+++ src/main/java/com/myapp/spring/security/configuration/MySecurityInitializer.java
@@ -0,0 +1,6 @@
+package com.myapp.spring.security.configuration;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class MySecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+}
